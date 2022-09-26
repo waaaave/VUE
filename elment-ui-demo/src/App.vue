1c64e0cc8782f0e4dae83486c2312ce342675b78
wave <1076544270@qq.com>
2022-09-26 17:01:56 +0800

    el-badge

--- elment-ui-demo/src/App.vue
+++ elment-ui-demo/src/App.vue
@@ -1,32 +1,39 @@
 <template>
-<div>
-  <el-button-group>
-  <el-button loading @click="handlePre" type="primary" icon="el-icon-arrow-left">
-    上一页
-  </el-button>
-  <el-button disabled type="primary">
-    下一页<i class="el-icon-arrow-right"></i>
-  </el-button>
-</el-button-group>  
-  <el-button plain>111</el-button>
-  <el-button round>222</el-button>
-  <el-button circle>33</el-button>
-</div>
+  <div>
+    <el-button-group>
+      <el-button loading @click="handlePre" type="primary" icon="el-icon-arrow-left">
+        上一页
+      </el-button>
+      <el-button disabled type="primary">
+        下一页<i class="el-icon-arrow-right"></i>
+      </el-button>
+    </el-button-group>
+    <el-button plain>111</el-button>
+    <el-button round>222</el-button>
+    <el-button circle>33</el-button>
+    <el-badge is-dot class="item">数据查询</el-badge>
+    <el-badge is-dot class="item">
+      <el-button class="share-button" icon="el-icon-share" type="primary"></el-button>
+    </el-badge>
+
+  </div>
 </template>
 
 <script>
-  import ElButtonGroup from '../components/ElButtonGroup.vue';
-  import ElButton from '../components/ElButton.vue';
+import ElButtonGroup from '../components/ElButtonGroup.vue';
+import ElButton from '../components/ElButton.vue';
+import ElBadge from '../components/ElBadge.vue';
 export default {
-  
-  methods:{
-    handlePre(e){
+
+  methods: {
+    handlePre(e) {
       console.log(e);
     }
   },
-  components:{
-    'el-button-group':ElButtonGroup,
-    'ElButton': ElButton
+  components: {
+    'el-button-group': ElButtonGroup,
+    'el-button': ElButton,
+    'el-badge': ElBadge
   }
 }
 </script>
